a04ca1da1085187dab98a65b1bdfd6d8b80512f8
C Lewis <casserole.lewis@gmail.com>
2023-02-24 11:37:51 -0500

    JS functionality
    Added functions

--- index.js
+++ index.js
@@ -4,29 +4,59 @@
 1 kilogram = 2.204 pound
 */
 
-//! On button click we receive input value
-//! Input value should be a number and greater than 0
+/****** VARIABLES ******/
 
-//! Convert value from text input field 
-//! Three different outputs with a formula for each one
-
-
-//VARIABLES
-
+//BUTTON
 const convertBtn = document.getElementById("convert");
 
+//TEXT INPUT FIELD
 const inputNum = document.getElementById("num-val");
+
+//PARAGRAPHS FOR CONVERSION CONTENT
 const metersToFeet = document.getElementById("m-ft");
 const litersToGallons = document.getElementById("l-gal");
 const kiloToPounds = document.getElementById("kg-lbs");
 
 
-//FUNCTIONS
+/****** FUNCTIONS ******/
 
-const convert = () => 
-    console.log("Button is clicked!");
 
+//! Can I use parameters and variables to make this DRYer?
+
+const convertLength = () => { 
+    return metersToFeet.textContent = 
+    `${inputNum.value} meters = 
+    ${Math.round((inputNum.value * 3.2808) * 1000) / 1000} feet |
+    ${inputNum.value} feet = 
+    ${Math.round((inputNum.value / 3.281) * 1000) / 1000} meters`
+};    
+
+const convertVolume = () => {
+    return litersToGallons.textContent = 
+    `${inputNum.value} liters = 
+    ${Math.round((inputNum.value * 0.2642) * 1000) / 1000} gallons |
+    ${inputNum.value} gallons = 
+    ${Math.round((inputNum.value / 0.264) * 1000) / 1000} liters`
+};
+
+const convertMass = () => { 
+    return kiloToPounds.textContent = 
+    `${inputNum.value} kilos = 
+    ${Math.round((inputNum.value * 2.2046) * 1000) / 1000} pounds |
+    ${inputNum.value} pounds = 
+    ${Math.round((inputNum.value / 2.204) * 1000) / 1000} kilos`
+}
+
+const reset = () =>
+    inputNum.value = "";    
  
-//EVENT LISTENERS
 
-convertBtn.addEventListener("click", convert());
+    /****** EVENT LISTENERS ******/
+
+//? Is there a way to have less event listeners?
+//? I had to separate them into 4 separate instances to get them to work
+
+convertBtn.addEventListener("click", convertLength); 
+convertBtn.addEventListener("click", convertVolume);
+convertBtn.addEventListener("click", convertMass);
+convertBtn.addEventListener("click", reset);
