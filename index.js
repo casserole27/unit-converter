67ce0898c971549cb30f0b98d4fece696d95427f
C Lewis <casserole.lewis@gmail.com>
2023-02-24 10:49:16 -0500

    JavaScript
    Initial JS - define variables and event listeners

--- index.js
+++ index.js
@@ -3,3 +3,30 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+//! On button click we receive input value
+//! Input value should be a number and greater than 0
+
+//! Convert value from text input field 
+//! Three different outputs with a formula for each one
+
+
+//VARIABLES
+
+const convertBtn = document.getElementById("convert");
+
+const inputNum = document.getElementById("num-val");
+const metersToFeet = document.getElementById("m-ft");
+const litersToGallons = document.getElementById("l-gal");
+const kiloToPounds = document.getElementById("kg-lbs");
+
+
+//FUNCTIONS
+
+const convert = () => 
+    console.log("Button is clicked!");
+
+ 
+//EVENT LISTENERS
+
+convertBtn.addEventListener("click", convert());
